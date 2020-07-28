ade4e29464d2d2c68f7e14acd082abcbf03c9a4a
Akinyemi vek <kinyemivek@gmail.com>
2020-07-27 21:07:45 -0700

    I added myName component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <myName name="Akinyemi Vek"/>
+  </div>
+</template>
+
+<script>
+import myName from './components/myName.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myName
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #111111;
+  color: #EEEEEE;
+  margin: 0px;
+  padding: 255px;
+}
+</style>
---
+++ src/components/myName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1> Hi, my name is <strong>{{ name }}</strong>.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'name'
+  ]
+} 
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 50px;
+  } 
+</style>
